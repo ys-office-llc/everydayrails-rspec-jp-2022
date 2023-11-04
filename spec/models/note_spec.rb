93598e54f1d3abdf5fb1d053e4c9a190f5121259
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-11-04 18:07:01 +0900

    3. モデルスペック - p47 - 失敗ケース

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  # 検索文字列に一致するメモを返すこと
   it "returns notes that match the search term" do
     user = User.create(
       first_name: "Joe",
@@ -28,5 +29,33 @@ RSpec.describe Note, type: :model do
 
     expect(Note.search("first")).to include(note1, note3)
     expect(Note.search("first")).to_not include(note2)
+  end
+  # 検索結果が1件も見つからなければ空のコレクションを返すこと
+  it "returns an empty collection when no results are found" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    project = user.projects.create(
+      name: "Test Project",
+    )
+
+    note1 = project.notes.create(
+      message: "This is the first note.",
+      user: user,
+    )
+    note2 = project.notes.create(
+      message: "This is the second note.",
+      user: user,
+    )
+    note3 = project.notes.create(
+      message: "First, preheat the oven.",
+      user: user,
+    )
+
+    expect(Note.search("message")).to be_empty
   end
 end
