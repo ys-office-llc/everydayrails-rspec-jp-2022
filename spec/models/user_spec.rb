abdb63284c4e55c117379f4eac96a278127e45ff
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2023-11-04 22:56:31 +0900

    rubocop 対応

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,62 +1,65 @@
+# rubocop:disable Style/AsciiComments, Style/FrozenStringLiteralComment
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User, type: :model do # rubocop:disable Metrics/BlockLength
   # 有効なファクトリを持つこと
-  it "has a valid factory" do
+  it 'has a valid factory' do
     expect(FactoryBot.build(:user)).to be_valid
   end
 
-  it "is valid with a first name, last name and email, and password" do
+  it 'is valid with a first name, last name and email, and password' do
     user = User.new(
-      first_name: "Aaron",
-      last_name:  "Sumner",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      first_name: 'Aaron',
+      last_name: 'Sumner',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze'
     )
     expect(user).to be_valid
   end
 
-  it "is invalid without a first name" do
+  it 'is invalid without a first name' do
     user = User.new(first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
-  it "is invalid without a last name" do
+  it 'is invalid without a last name' do
     user = User.new(last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
-  it "is invalid without an email address" do
+  it 'is invalid without an email address' do
     user = User.new(email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
-  it "is invalid with a duplicate email address" do
+  it 'is invalid with a duplicate email address' do
     User.create(
-      first_name:  "Joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze'
     )
     user = User.new(
-      first_name:  "Jane",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+      first_name: 'Jane',
+      last_name: 'Tester',
+      email: 'tester@example.com',
+      password: 'dottle-nouveau-pavilion-tights-furze'
     )
     user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    expect(user.errors[:email]).to include('has already been taken')
   end
 
   it "returns a user's full name as a string" do
     user = User.new(
-      first_name: "John",
-      last_name:  "Doe",
-      email:      "johndoe@example.com",
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'johndoe@example.com'
     )
-    expect(user.name).to eq "John Doe"
+    expect(user.name).to eq 'John Doe'
   end
 end
+
+# rubocop:enable Style/AsciiComments, Style/FrozenStringLiteralComment
